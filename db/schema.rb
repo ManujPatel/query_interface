459e3c7f08e0425ae975be7ee3d50906bb182eca
Manuj Patel <manuj.p@simformsolutions.com>
2022-02-10 23:09:13 +0530

    added employee details and show employees with diffrent conditions,added lock_version to employee,added search with email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_09_112712) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_10_171753) do
   create_table "employees", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_09_112712) do
     t.float "salary"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "lock_version"
   end
 
 end
